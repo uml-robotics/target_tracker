81dbaed53b315dbeb904b34a9efb9d035bba4072
Mikhail Medvedev <mmedvede@cs.uml.edu>
2013-12-23 18:54:32 -0500

    adds Target class

---
+++ src/target.cpp
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013, University of Massachusetts Lowell.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of University of Massachusetts Lowell. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Author: Mikhail Medvedev */
+#include "target.h"
+#include "ros/ros.h"
+
+namespace target_tracker
+{
+
+  Target::Target(double x, double y, double radius) :
+      radius_(radius), active_(true)
+  {
+    pose_.position.x = x;
+    pose_.position.y = y;
+    pose_.orientation.w = 1; // Orientation is not used
+  }
+  typedef std::vector<Target> vector;
+
+  geometry_msgs::Pose & Target::getPose()
+  {
+    return pose_;
+  }
+
+  bool Target::isActive() const
+  {
+    return active_;
+  }
+
+  void Target::setActive(bool state)
+  {
+    active_ = state;
+    ROS_WARN_STREAM(active_);
+  }
+
+} /* namespace target_tracker */
